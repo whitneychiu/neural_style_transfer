50a42777596154846190e9e85905091de973ebdf
Huei-Chen Whitney Chiu <57679474+whitneychiu@users.noreply.github.com>
2023-08-07 19:54:13 -0700

    initial commit

---
+++ style_modules/style_loss.py
@@ -0,0 +1,95 @@
+import torch
+import torch.nn as nn
+
+class StyleLoss(nn.Module):
+    def gram_matrix(self, features, normalize=True):
+        """
+            Compute the Gram matrix from features.
+
+            Inputs:
+            - features: PyTorch Variable of shape (N, C, H, W) giving features for
+              a batch of N images.
+            - normalize: optional, whether to normalize the Gram matrix
+                If True, divide the Gram matrix by the number of neurons (H * W * C)
+
+            Returns:
+            - gram: PyTorch Variable of shape (N, C, C) giving the
+              (optionally normalized) Gram matrices for the N input images.
+            """
+        ##############################################################################
+        # TODO: Implement style loss function                                        #
+        # Please pay attention to use torch tensor math function to finish it.       #
+        # Otherwise, you may run into the issues later that dynamic graph is broken  #
+        # and gradient can not be derived.                                           #
+        #                                                                            #
+        # HINT: you may find torch.bmm() function is handy when it comes to process  #
+        # matrix product in a batch. Please check the document about how to use it.  #
+        ##############################################################################
+        N, C, H, W = features.shape
+        # print("N, C , H, W", N, C , H, W)
+
+        feature_map = features.reshape(N, C, -1)
+        # print(feature_map[0].shape)
+        # print(feature_map[0][0].shape)
+        # print("feature_map", feature_map)
+
+        feature_map_t = torch.transpose(feature_map, 1, 2)
+        gram = torch.bmm(feature_map, feature_map_t)
+
+        if normalize == True:
+          gram = gram/ (C* H* W)
+        
+        return gram
+
+        
+
+        ##############################################################################
+        #                             END OF YOUR CODE                               #
+        ##############################################################################
+    def forward(self, feats, style_layers, style_targets, style_weights):
+        """
+           Computes the style loss at a set of layers.
+
+           Inputs:
+           - feats: list of the features at every layer of the current image, as produced by
+             the extract_features function.
+           - style_layers: List of layer indices into feats giving the layers to include in the
+             style loss.
+           - style_targets: List of the same length as style_layers, where style_targets[i] is
+             a PyTorch Variable giving the Gram matrix the source style image computed at
+             layer style_layers[i].
+           - style_weights: List of the same length as style_layers, where style_weights[i]
+             is a scalar giving the weight for the style loss at layer style_layers[i].
+
+           Returns:
+           - style_loss: A PyTorch Variable holding a scalar giving the style loss.
+           """
+
+        ##############################################################################
+        # TODO: Implement style loss function                                        #
+        # Please pay attention to use torch tensor math function to finish it.       #
+        # Otherwise, you may run into the issues later that dynamic graph is broken  #
+        # and gradient can not be derived.                                           #
+        #                                                                            #
+        # Hint:                                                                      #
+        # you can do this with one for loop over the style layers, and should not be #
+        # very much code (~5 lines). Please refer to the 'style_loss_test' for the   #
+        # actual data structure.                                                     #
+        #                                                                            #
+        # You will need to use your gram_matrix function.                            #
+        ##############################################################################
+        style_loss = torch.autograd.Variable(torch.tensor(0.))
+
+        for i in range(len(style_layers)):
+          gram = self.gram_matrix(feats[style_layers[i]]) 
+          layer_style_loss = style_weights[i] * torch.sum(torch.pow(gram - style_targets[i], 2))
+
+          style_loss += layer_style_loss
+        return style_loss
+
+
+
+        ##############################################################################
+        #                             END OF YOUR CODE                               #
+        ##############################################################################
+
